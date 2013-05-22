b502ce6d9283069677556e4bde143a072eba5e0d
tokuhirom <tokuhirom@gmail.com>
2013-05-22 19:09:22 +0900

    suppress warnings

--- lib/Perl/MinimumVersion/Fast.pm
+++ lib/Perl/MinimumVersion/Fast.pm
@@ -130,7 +130,9 @@ sub _build_minimum_syntax_version {
 
 sub minimum_version {
     my $self = shift;
-    return max($self->{minimum_explicit_version} || 0, $self->{minimum_syntax_version});
+    return $self->{minimum_explicit_version} > $self->{minimum_syntax_version}
+        ? $self->{minimum_explicit_version}
+        : $self->{minimum_syntax_version};
 }
 
 sub minimum_syntax_version {
