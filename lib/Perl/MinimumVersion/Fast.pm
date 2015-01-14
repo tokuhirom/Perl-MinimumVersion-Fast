44553ef0c4bdbfb7e85052bdcdaab161c80b2c59
brian d foy <brian.d.foy@gmail.com>
2015-01-14 06:29:41 -0500

    I think you meant the processed @tokens, not the one passed in

--- lib/Perl/MinimumVersion/Fast.pm
+++ lib/Perl/MinimumVersion/Fast.pm
@@ -48,7 +48,7 @@ sub _build_minimum_explicit_version {
     my $explicit_version;
     for my $i (0..@tokens-1) {
         if ($tokens[$i]->{name} eq 'UseDecl' || $tokens[$i]->{name} eq 'RequireDecl') {
-            if (@$tokens >= $i+1) {
+            if (@tokens >= $i+1) {
                 my $next_token = $tokens[$i+1];
                 if ($next_token->{name} eq 'Double' or $next_token->{name} eq 'VersionString') {
                     $explicit_version = max($explicit_version || 0, version->new($next_token->{data}));
